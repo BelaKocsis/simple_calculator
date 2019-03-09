afde23a56b5c19fcb2a9a64ad023d8c986abed12
Bela Kocsis <siro2005@me.com>
2019-03-09 15:52:57 +0100

    Adding multiplication

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,4 @@
 a = int(input("Please, give a number: "))
 b = int(input("Please, give another number: "))
 print("The sum of the numbers is: ", a+b)
+print("The multiplication of the numbers is: ", a*b)
